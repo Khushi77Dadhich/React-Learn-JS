0d9cd636e0d9a745718bc40288933e29d417854d
Khushi77Dadhich <138347837+Khushi77Dadhich@users.noreply.github.com>
2024-04-13 10:14:05 +0000

    We are learning react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+import Chai from './Chai.js'
+
+function App() {
+  return (
+    <>
+    <Chai />
+    <h1>Hello It's Khushi World! we are here going to start our new journey of learning react. So lets get started.....</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,15 @@
+function Chai() {
+    return(
+        <h2>Chai in react</h2>
+    )
+}
+
+export default Chai;
+
+
+
+//some important point you should remember 
+//keep your function name capital 
+//keep your file name also capital not important but its a good practice
+//some time in normal react if any html is returning then we should keep 
+//our file name as JSX or if javascript is retuning then keep its name as JS only
